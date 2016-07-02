e0a3f16751549d94121a583eae1f86d71a3f905c
Quincy Reid <qreid51@gmail.com>
2016-07-02 10:45:44 -0400

    Adds s3 api request

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-// Get the packages we need
 var express = require('express')
 var bodyParser = require('body-parser')
-// Create our Express application
+var AWS = require('aws-sdk')
 var app = express()
-
-// Create our Express router
 var router = express.Router()
+var s3 = new AWS.S3()
 
-// Use the body-parser package in our application
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   type: 'application/json',
   extended: true
 }))
 
-// Register all our routes with /api
 app.use('/api', router)
 
-// Start the server
 if (!module.parent) {
   app.listen(process.env.PORT || 3000)
 }
 
 router.post('/submit', (req, res) => {
   res.send({message: 'This is working', body: req.body})
-  console.log('this works')
-  console.log(req.body)
+  var thisBody = JSON.stringify(req.body)
+  var params = {
+    Bucket: 'contact-lists',
+    Key: 'my-recent-contacts',
+    Body: thisBody
+  }
+  s3.putObject(params, (err, data) => {
+    if (err) return err
+    console.log('Successfully uploaded data')
+  })
 })
+
+module.exports = app
